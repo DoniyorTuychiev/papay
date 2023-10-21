a58f41ae00bcd2a5543a1f25f8af806eaf5b59fe
doni <doni0101195@gmail.com>
2023-10-21 19:34:20 +0900

    feat: multer single image upload process

--- controllers/productController.js
+++ controllers/productController.js
@@ -13,9 +13,9 @@ productController.getAllProducts = async (req, res) => {
 productController.addNewProduct = async (req, res) => {
     try{
         console.log("POST: cont/addNewProduct");
-        console.log(req.member);
+        // console.log(req.member);
       //TODO: product creation develop
-      
+      res.send("ok");
       }catch(err){
         console.log(`ERROR: cont/addNewProduct, ${err.message}`);
        
