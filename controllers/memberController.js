89e002312ddb21e6804238ab1a45df7ddf717c71
doni <doni0101195@gmail.com>
2023-12-01 16:53:36 +0900

    feat: checkMyAuthentication and retrieve token object data va refresh_token tushunchasi by_71

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,12 +1,19 @@
 const assert = require("assert");
 const Member = require("../models/Member");
-const cookieParser = require("cookie-parser");
 
 let memberController = module.exports;
 
 const jwt = require("jsonwebtoken");
 const Definer = require("../lib/mistake");
 
+//jwt sign qilishda vaqtni albatta tekshiradi agar cookie va token amal qilish vaqti boshqa bolsa yani token ni vaqti qisqa bolsa va u tugagach req bolsa null qiymat qaytaradi
+//JsonWebToken dataBase ga save bolishi shart emas
+//refresh_token esa db ga save qilinadi.jwt amal qilish vaqti qisqa vaqtga yozilganda vaqti tugagach exespire yani amal  qilish vaqti tugadi deb bildirish keladi . 
+//Agar refresh_token dan foydalanilsa access_token vaqti togagach yana req bolsa brauzer db ga murojat qiladi va u yerdan refresh_token ni oladi. va shu orqali access_token yangilanadi     (access_token va refresh_token bor access_token qisqa vaqt bilan yaratiladi)
+//masalan: access_token 15 minut bolsa refresh_token 1 oy yoki 1 hafta va hakozo bolishi mumkin . Bunda har 15 minutda access_token tugap refresh_token orqali refresh bolip turadi.Bu holat refresh_tokenni amal qilish shuddatiga bogliq ravishda davom etadi
+//refresh_tokenni yana bir afzalligi agar sizni profilizga hacking bolaversa site adminiga yozasiz va u db dan refresh_token ni ochiradi
+//shundan song siz har safar site ga kirganizda login bolip turmaysiz va sizzni token lariz ham refresh bolib turmedi
+//bazi brauzerlarga kora httpS protokollar ga amal qilgan holdagina yani bazi shartlarga kora backend domen bilan frontend domeni yani ip adresi bir hil bolgandagina cookielar uzatiladi bu esa hackingni oldini olish uchun
 /**signup section start */
 memberController.signup = async (req, res) => {
   try{
@@ -65,16 +72,33 @@ memberController.createToken = (result) => {
     const upload_data = {
       _id: result._id,
       mb_nick: result.mb_nick,
-      mb_type: result.mb_type
+      mb_type: result.mb_type,
+      mb_phone: result.mb_phone,
+      mb_status: result.mb_status, //qanchalik malumot soralsa token uzayip ketadi
     };
 
-    const token = jwt.sign(upload_data, process.env.SECRET_TOKEN, {
+    const token = jwt.sign(upload_data, process.env.SECRET_TOKEN, { 
       expiresIn: "6h",
     });
-
     assert.ok(token, Definer.auth_err4);
     return token;
   }catch(err){
     throw err;
   }
 }
+
+memberController.checkMyAuthentication = (req, res) => {
+  try{
+    console.log("GET cont/checkMyAuthentication");
+    let token = req.cookies['access_token'];
+    console.log("token:::", token);
+
+    const member = token ? jwt.verify(token, process.env.SECRET_TOKEN) : null;
+
+    assert.ok(member, Definer.auth_err4);
+
+    res.json({ static:"succeed", data: member }); 
+  }catch(err){
+    throw err;
+  }
+};
